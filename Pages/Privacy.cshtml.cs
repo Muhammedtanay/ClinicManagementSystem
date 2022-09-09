16f7ace4543e95b5c2d3ce3f9181690cb3459008
Muhammed TANAY <86378012+Muhammedtanay@users.noreply.github.com>
2022-09-09 16:36:47 +0300

    Update

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -22,3 +22,19 @@ namespace WebApplication1.Pages
         }
     }
 }
+namespace WebApplication1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
