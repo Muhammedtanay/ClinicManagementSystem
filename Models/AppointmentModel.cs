777e8ea30b2c6df4d5dc7c9020a6df4259f758e9
Muhammed TANAY <86378012+Muhammedtanay@users.noreply.github.com>
2022-09-09 16:23:46 +0300

    Update

--- Models/AppointmentModel.cs
+++ Models/AppointmentModel.cs
@@ -12,3 +12,10 @@
         public int AppointmentId { get; set; }
     }
 }
+namespace WebApplication1.Models
+{
+    public class AppointmentModel
+    {
+        public int AppointmentId { get; set; }
+    }
+}
